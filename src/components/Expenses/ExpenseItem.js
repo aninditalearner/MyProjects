3817d533bd4c6afbf3264c1eb55322b7240b3248
Anindita Rakshit <aninditawork44@outlook.com>
2023-07-30 23:06:12 +0530

    Adding new expense form

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,16 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 const ExpenseItem = (props) => {
 
+    //let title = props.title;
+
+    //useState returns an array of two elements -> 1st one is the variable value itself and 2nd is teh func to update the value
+    const [title, setTitle] = useState(props.title);
+
+    const clickHandler =() => {
+        setTitle("Updated!");
+        console.log(title);
+        //title="Updated!";
+    }
     return (
         <Card className="expense-item">
             <ExpenseDate date = {props.date}/>
             <div className="expense-item__description">
-                <h2>{props.title}</h2>
+                <h2>{title}</h2>
                 <div className="expense-item__price">${props.price}</div>
             </div>
+            <button onClick={clickHandler}>Change title</button>
         </Card>
     );
 }
---
+++ src/components/NewExpense/NewExpense.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './NewExpense.css';
+
+const NewExpense = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default NewExpense;
