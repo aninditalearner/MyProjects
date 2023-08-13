9aad7234e3d1172c5d84a7c1da978e9153fb5d4e
Rakshit <arakshit@wiley.com>
2023-08-13 19:06:34 +0530

    years filter added

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,17 +1,27 @@
-import React from 'react';
+import React, { useState} from 'react';
 import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
 import "./Expenses.css";
 import ExpensesFilter from './ExpensesFilter';
 function Expenses(props) {
+    const [selectedYearFromDrodown,setSelectedYear] = useState('2021');
+    let excludedYears = '2019, 2020 & 2021';
     const expenseFilterHandler = (selectedYear) => {
-        const year = {
-            ...selectedYear
-        };
+        setSelectedYear(selectedYear);
+        if(selectedYearFromDrodown==='2019'){
+            excludedYears = '2020, 2021 & 2022';
+        }
+        else if(selectedYearFromDrodown==='2020'){
+            excludedYears = '2019, 2021 & 2022';
+        }
+        else if(selectedYearFromDrodown==='2021'){
+            excludedYears = '2019, 2020 & 2022';
+        }
     }
     return(
         <Card className="expenses">
-            <ExpensesFilter onSelect={expenseFilterHandler}/>
+            <ExpensesFilter selected={selectedYearFromDrodown} onSelect={expenseFilterHandler}/>
+            <p>Data for the year {excludedYears} is hidden</p>
             <ExpenseItem
             title={props.item[0].title}
             date={props.item[0].date}
